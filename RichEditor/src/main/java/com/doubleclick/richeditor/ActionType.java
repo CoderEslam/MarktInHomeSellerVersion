301318b3f9ba356dff9bfcf474968944982bb49e
eslam <eslamfgazy600@gmail.com>
2022-04-14 13:10:53 +0200

    Added Rich Editor

--- RichEditor/src/main/java/com/doubleclick/richeditor/ActionType.java
+++ RichEditor/src/main/java/com/doubleclick/richeditor/ActionType.java
@@ -14,24 +14,47 @@ public enum ActionType {
     NONE(0),
 
     // FONT
-    FAMILY(1), SIZE(2), LINE_HEIGHT(3), FORE_COLOR(4), BACK_COLOR(5),
+    FAMILY(1),
+    SIZE(2),
+    LINE_HEIGHT(3),
+    FORE_COLOR(4),
+    BACK_COLOR(5),
 
     // Format
-    BOLD(6), ITALIC(7), UNDERLINE(8), SUBSCRIPT(9), SUPERSCRIPT(10), STRIKETHROUGH(11),
+    BOLD(6),
+    ITALIC(7),
+    UNDERLINE(8),
+    SUBSCRIPT(9),
+    SUPERSCRIPT(10),
+    STRIKETHROUGH(11),
 
     // Style
-    NORMAL(12), H1(13), H2(14), H3(15), H4(16), H5(17), H6(18),
+    NORMAL(12),
+    H1(13),
+    H2(14),
+    H3(15),
+    H4(16),
+    H5(17),
+    H6(18),
 
     //Justify
-    JUSTIFY_LEFT(19), JUSTIFY_CENTER(20), JUSTIFY_RIGHT(21), JUSTIFY_FULL(22),
+    JUSTIFY_LEFT(19),
+    JUSTIFY_CENTER(20),
+    JUSTIFY_RIGHT(21),
+    JUSTIFY_FULL(22),
 
     // List Style
-    ORDERED(23), UNORDERED(24),
+    ORDERED(23),
+    UNORDERED(24),
 
-    INDENT(25), OUTDENT(26),
+    INDENT(25),
+    OUTDENT(26),
 
     // Insert
-    IMAGE(27), LINK(28), TABLE(29), LINE(30),
+    IMAGE(27),
+    LINK(28),
+    TABLE(29),
+    LINE(30),
 
     BLOCK_QUOTE(31), BLOCK_CODE(32),
 
